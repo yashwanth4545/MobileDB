be8f1d82f8f3101578cee79472c32b715f945699
yashwanth4545 <83054115+yashwanth4545@users.noreply.github.com>
2026-04-02 00:26:17 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Project level build.gradle.kts
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+}
